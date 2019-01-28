e30977d59ef38e336e11a1686a9d37ad8ead9c06
Mark <contact.mshin@gmail.com>
2019-01-27 21:59:25 -0700

    google api linked

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -7,7 +7,23 @@ module.exports = {
     findAll: (req, res) => {
         const { query : params } = req;
         axios.get("https://www.googleapis.com/books/v1/volumes", { params })
-            .then( results => results.data.items )
-            .then()
+            .then( results => results.data.items.filter(
+                result => 
+                    result.volumeInfo.title &&
+                    result.volumeInfo.infoLink &&
+                    result.volumeInfo.authors &&
+                    result.volumeInfo.description &&
+                    result.volumeInfo.imageLinks &&
+                    result.volumeInfo.imagesLinks.thumbnail
+            ))
+            .then( apiBooks => 
+                db.Book.find().then( dbBooks =>
+                    apiBooks.filter( apiBook =>
+                        dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
+                    )
+                )
+            )
+            .then( books => res.json(books))
+            .catch(err => res.status(404).json(err))
     }
-}
+};
