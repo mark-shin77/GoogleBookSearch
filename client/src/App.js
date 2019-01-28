93af2cd521850af0daaae211333bae79a6b4173a
Mark <contact.mshin@gmail.com>
2019-01-27 23:33:55 -0700

    front end routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react"
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from "./pages/Main";
+import Saved from "./pages/Saved";
+import NotFound from "./pages/NotFound";
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/saved" component={Saved} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
