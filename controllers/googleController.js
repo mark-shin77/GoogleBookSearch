57e4fa9e7ea6c989bf29384a9595b7210df90283
Mark <contact.mshin@gmail.com>
2019-01-27 23:24:08 -0700

    starting front end

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -1,14 +1,10 @@
-// Dependencies
-const axios = require("axios");
-const db = require("../models");
-
-// Defining CRUB methods for google , Filter function used to make sure we only get new books
+// Defining CRUB methods for google
 module.exports = {
     findAll: (req, res) => {
         var books = require('google-books-search');
         books.search('Professional JavaScript for Web Developers', function (error, results) {
             if (!error) {
-                res.json(results);
+                res.json(results)
             } else {
                 console.log(error);
             }
