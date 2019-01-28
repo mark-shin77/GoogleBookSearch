4e33bd065370d6394835ca2996323c49d05ce39e
Mark <contact.mshin@gmail.com>
2019-01-27 23:42:42 -0700

    main / saved pages started

---
+++ client/src/pages/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+    render () {
+        return (
+            <div>
+                <h1>This is the main page</h1>
+            </div>
+        )
+    }
+}
+
+export default Main;
---
+++ client/src/pages/saved.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Saved extends Component {
+    render () {
+        return (
+            <div>
+                <h1>This is the saved page</h1>
+            </div>
+        )
+    }
+}
+
+export default Saved;
